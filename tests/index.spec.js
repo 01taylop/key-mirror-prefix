1eb5920af7d66db42520a47e603b0794c77fb7ac
Patrick Taylor <hello@patricktaylor.dev>
2024-05-25 19:05:13 +0100

    Use Jest for the unit tests 🃏

---
+++ tests/index.spec.js
@@ -0,0 +1,49 @@
+import assert from 'assert'
+
+import keyMirrorPrefix from '../'
+
+describe('keyMirrorPrefix', () => {
+
+  it('throws an error if the second argument is not an object', () => {
+    expect(() => {
+      keyMirrorPrefix(null, null)
+    }).toThrow('keyMirrorPrefix(...): Argument must be an object.')
+  })
+
+  it('throws an error if the second argument is an array', () => {
+    expect(() => {
+      keyMirrorPrefix(null, [])
+    }).toThrow('keyMirrorPrefix(...): Argument must be an object.')
+  })
+
+  it('returns an empty object if the second argument is an empty object', () => {
+    const testObject = keyMirrorPrefix(null, {})
+
+    expect(testObject).toEqual({})
+  })
+
+  it('returns a key mirror object with no prefix', () => {
+    const testObject = keyMirrorPrefix(null, {
+      hello: null,
+      world: null,
+    })
+
+    expect(testObject).toEqual({
+      hello: 'hello',
+      world: 'world',
+    })
+  })
+
+  it('returns a key mirror object with a prefix', () => {
+    const testObject = keyMirrorPrefix('prefix', {
+      hello: null,
+      world: null,
+    })
+
+    expect(testObject).toEqual({
+      hello: 'prefix_hello',
+      world: 'prefix_world',
+    })
+  })
+
+})
