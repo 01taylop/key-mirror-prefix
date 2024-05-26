5ad666e35be1820776c64f86695dfe4310e91b69
Patrick Taylor <01taylop@users.noreply.github.com>
2024-05-26 11:44:19 +0100

    Upgraded key-mirror-prefix 💫 (#1)
    * Repo maintenance 🏡
    
    * Repo maintenance 🏚
    
    * Build bundle for commonJS and ES 🏗️
    
    * Convert keyMirrorPrefix to a named export 📛
    
    * Revert testing strategy 🫡
    
    * Test compiled code 🌟
    
    * Only test Node version which Jest supports 😲
    
    * Support `keyMirror` function too 🪞
    
    * Added rimraf to depcheck 🗑️
    
    * Use prepublishOnly script 📦
    
    * Added a PR template 📝
    
    * Use Yarn in build process 🧶
    
    * Updated README.md 📗
    
    * Build before testing bundles 👷🏻‍♂️

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const keyMirrorPrefix = (prefix, obj) => {
+  if (!(obj instanceof Object && !Array.isArray(obj))) {
+    throw new Error('keyMirrorPrefix(...): Second argument must be an object.')
+  }
+
+  const mirror = {}
+  Object.keys(obj).forEach(key => {
+    mirror[key] = prefix ? `${prefix}_${key}` : key
+  })
+
+  return mirror
+}
+
+const keyMirror = obj => keyMirrorPrefix(null, obj)
+
+export {
+  keyMirror,
+  keyMirrorPrefix,
+}
