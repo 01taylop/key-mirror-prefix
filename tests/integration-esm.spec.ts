222b183ffc5a027afc5b6ab5f9df8d9751718aac
Patrick Taylor <hello@patricktaylor.dev>
2026-01-02 20:00:57 +0000

    Added integration tests 🍾

---
+++ tests/integration-esm.spec.ts
@@ -0,0 +1,17 @@
+import { keyMirror, keyMirrorPrefix } from '../lib/index.js'
+
+describe('Integration tests - ESM', () => {
+
+  it('keyMirror works correctly', () => {
+    const result = keyMirror({ a: null, b: null })
+
+    expect(result).toEqual({ a: 'a', b: 'b' })
+  })
+
+  it('keyMirrorPrefix works correctly', () => {
+    const result = keyMirrorPrefix('test', { x: null })
+
+    expect(result).toEqual({ x: 'test_x' })
+  })
+
+})
